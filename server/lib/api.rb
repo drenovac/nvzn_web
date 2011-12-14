33d9f727e1581ce1a801335f862f06ef28fde423
geoffreyd <geoffreyd@gmail.com>
2011-12-14 14:49:07 +1000

    update API to support couchDB pass through and users stored in Couch.

--- server/lib/api.rb
+++ server/lib/api.rb
@@ -4,78 +4,99 @@ require 'activerecord-jdbc-adapter'
 require './lib/sqljdbc4.jar'
 
 require 'sinatra'
+require 'rest_client'
 require 'json'
+require 'digest/sha2'
 
-# Serve up static assets with a long expiration header
+set :sessions, true
+
+COUCH = 'http://rectertupochastroyeysery:OKeJh8V1Rj8ABqXCvElfUCMj@geoffreyd.cloudant.com/nvzn'
 
 config = {
-  :url => "jdbc:sqlserver://110.175.29.142;databaseName=NVZN11",
-  :adapter => "jdbc",
-  :username => "sa",
-  :password => "s1nemojP0werforce",
-  :driver => 'com.microsoft.sqlserver.jdbc.SQLServerDriver'
+    :url => "jdbc:sqlserver://10.1.1.50;databaseName=NVZN11",
+    :adapter => "jdbc",
+    :username => "sa",
+    :password => "s1nemojP0werforce",
+    :driver => 'com.microsoft.sqlserver.jdbc.SQLServerDriver'
 }
 
 use Rack::Deflater
 
+get '/api/v1.1/login' do
+  halt 401, {'status' => "not authorised"}.to_json if !session || session[:username].blank?
+  
+  url = "#{COUCH}/#{session[:username]}"
+  
+  begin
+    data = '{"status":"ok", "user":'+RestClient.get(url)+"}"
+  rescue => e
+    e.response
+  end
+  
+end
+
 post '/api/v1.1/login' do
   request.body.rewind  # in case someone already read it
-  data = JSON.parse request.body.read
-  username = data['username']
-  if username === 'site'
-    user = {
-      :status => "ok",
-      :user => {
-        :id   => 1,
-        :name => 'NGVI1',
-        :role => 'site'
-      }
-    }
-  elsif username === 'employee'
-    user = {
-      :status => "ok",
-      :user => {
-        :id   => 12450,
-        :name => 'Mr Employee',
-        :role => 'employee'
-      }
-    }
-  else
-    status 401
-    user = { :status => 'unauthorised' }
-  end
+  post = JSON.parse request.body.read
+  username = post['username']
+  password = Digest::SHA2.hexdigest(post['password'])
 
-  body user.to_json
+  url = "#{COUCH}/#{username}"
+  
+  begin
+    data = JSON.parse(RestClient.get(url))
+    puts "logging in: #{url}"
+    
+    halt 401, { :status => 'unauthorised', :data => data }.to_json unless data["reason"].blank?
+    halt 401, { :status => 'unauthorised', :password => password, :data => data, :post => post }.to_json if data["password"] != password
+    
+    session[:username] = username
+    session[:id] = data["db_id"]
+    body ({ :status => "ok", :user => data }).to_json
+    
+  rescue => e
+    e.response
+  end
 end
 
 get '/api/v1.1/site/timecards' do
+  
+  halt 401, {'status' => "not authorised"}.to_json if session[:username].blank?
 
   ActiveRecord::Base.establish_connection( config )
   connection = ActiveRecord::Base.connection
 
   #customer = params[:customer]
-  customer = 'NGVI1'
-  #this_monday = Date.today.monday.to_formatted_s("%d %d %Y")
-  #next_friday = Date.today.next_week(:sunday).to_formatted_s("%d %d %Y")
-  this_monday = '2010-09-27'
-  next_friday = '2010-10-03'
+  customer = session[:id]
+  if !params[:week] || params[:week] == 0
+    date = Date.today
+  else
+    date = Date.today + params[:week].to_i.weeks
+  end
+  this_monday = date.monday.to_formatted_s("%Y-%m-%d")
+  next_sunday = date.sunday.to_formatted_s("%Y-%m-%d")
+  # this_monday = '2010-09-27'
+  # next_friday = '2010-10-03'
 
   sql =<<SQL
     SELECT ROSTER_TIMECARD.roster_date, ROSTER_TIMECARD.customer, ROSTER_TIMECARD.employee,
-    ROSTER_TIMECARD.ftime, ROSTER_TIMECARD.stime, employee.surname, employee.first_name
+    ROSTER_TIMECARD.ftime, ROSTER_TIMECARD.stime, employee.surname, employee.first_name,
+    employee.photo_path, employee.contact_numbers,
+    employee.employee_a_street, employee.employee_a_suburb
     FROM  ROSTER_TIMECARD INNER JOIN
           EMPLOYEE ON ROSTER_TIMECARD.employee = EMPLOYEE.employee INNER JOIN
           CNA ON ROSTER_TIMECARD.customer = CNA.code
 
-    WHERE ROSTER_DATE BETWEEN '#{this_monday}' AND '#{next_friday}'
+    WHERE ROSTER_DATE BETWEEN '#{this_monday}' AND '#{next_sunday}'
     AND  customer = '#{customer}'
     ORDER BY customer,  employee, roster_date, stime, ftime
 SQL
-  # WHERE roster_date >= '#{this_monday}' AND roster_date <= '#{next_friday}'
 
   response = {
       :name => customer,
-      :employees => {}
+      :employees => {},
+      :start => this_monday,
+      :finish => next_friday
   }
   results = connection.execute(sql)
   employees = {}
@@ -86,6 +107,9 @@ SQL
     employees[employee_id] ||= {
       :first_name => r['first_name'],
       :last_name => r['surname'],
+      :photo_path=> r['photo_path'],
+      :contact_numbers => r['contact_numbers'].to_s.split('<vm/>'),
+      :address => [r['employee_a_street'],r['employee_a_suburb']].compact.reject(&:blank?).join(", "),
       :id => employee_id,
       :timeCards => []
     }
@@ -159,6 +183,30 @@ SQL
 
 end
 
-get 'favicon.ico' do
+get '/api/:path' do
+  path = params[:path]
+  url = "#{COUCH}/#{path}"
+  puts "GET #{url}"
+  begin
+    data = RestClient.get url
+  rescue => e
+    e.response
+  end
+end
+
+post '/api/:path' do
+  path = params[:path]
+  # this is a preferences request
+  url = "#{DB}/#{path}"
+  puts "POST #{url}"
+  puts request.body.string
+  begin
+    data = RestClient.post url, request.body
+  rescue => e
+    e.response
+  end
+end
+
+get '/favicon.ico' do
   return
 end
