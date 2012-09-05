a2090faa5d82dbd134f39883c4961476cbfd2aa7
geoffreyd <geoffreyd@gmail.com>
2012-09-05 17:15:08 +1000

    Fix formatting in main_page.js

--- apps/nvzn/resources/main_page.js
+++ apps/nvzn/resources/main_page.js
@@ -2,199 +2,194 @@ Nvzn.mainPage = SC.Page.create({
 
   tabView: SC.outlet('mainPane.pageView.mainView.contentView.tabView'),
 
-    mainPane:SC.MainPane.design({
-      calendar: SC.outlet('sidebarView.calendarView.calendarView'),
-      tabView: SC.outlet('pageView.mainView.contentView.tabView'),
-      childViews:'sidebarView pageView'.w(),
+  mainPane:SC.MainPane.design({
+    calendar: SC.outlet('sidebarView.calendarView.calendarView'),
+    tabView: SC.outlet('pageView.mainView.contentView.tabView'),
+    childViews:'sidebarView pageView'.w(),
 
-      pageView:SC.View.extend({
-        classNames:'page'.w(),
+    pageView:SC.View.extend({
+      classNames:'page'.w(),
 
-        layout:{
-          left:194, top:4, bottom:4, right:4,
+      layout:{
+        left:194, top:4, bottom:4, right:4,
 
-          minWidth:875
-        },
+        minWidth:875
+      },
 
-        childViews:'shadowView mainView'.w(),
+      childViews:'shadowView mainView'.w(),
 
-        shadowView:SC.View.extend({
-          classNames:'page-shadow'.w(),
-          render:function (context) {
-            context.begin('div').addClass('top-left').end();
-            context.begin('div').addClass('top').end();
-            context.begin('div').addClass('top-right').end();
-            context.begin('div').addClass('left').end();
-            context.begin('div').addClass('right').end();
-            context.begin('div').addClass('bottom-left').end();
-            context.begin('div').addClass('bottom').end();
-            context.begin('div').addClass('bottom-right').end();
-          }
+      shadowView:SC.View.extend({
+        classNames:'page-shadow'.w(),
+        render:function (context) {
+          context.begin('div').addClass('top-left').end();
+          context.begin('div').addClass('top').end();
+          context.begin('div').addClass('top-right').end();
+          context.begin('div').addClass('left').end();
+          context.begin('div').addClass('right').end();
+          context.begin('div').addClass('bottom-left').end();
+          context.begin('div').addClass('bottom').end();
+          context.begin('div').addClass('bottom-right').end();
+        }
         }),
 
-        mainView:SC.View.extend({
-          classNames:'main'.w(),
-          layout:{ top:9, left:17, right:12, bottom:9 },
+      mainView:SC.View.extend({
+        classNames:'main'.w(),
+        layout:{ top:9, left:17, right:12, bottom:9 },
 
-          childViews:'headerView contentView footerView'.w(),
+        childViews:'headerView contentView footerView'.w(),
 
-          //// HEADER
-          headerView:SC.View.extend({
-            classNames:'page-header'.w(),
-            layout:{
-              height:100
-            },
-            childViews:'nameLabel nameView dateLabel dateView contactLabel contactView greetingLabel logoutButton'.w(),
-            nameLabel:SC.View.extend({
-              classNames:'label project'.w(),
-              isVisibleBinding:SC.Binding.from('Nvzn.isSite'),
-              layout:{
-                top:35, left:40, width:110, height:18},
-              render:function (context, firstTime) {
-                return context.text('Name:');
-              }
-            }),
-            nameView:SC.View.extend(SC.ContentDisplay, {
-              classNames:'header-name'.w(),
-              displayProperties:'content'.w(),
-              contentDisplayProperties:'displayName'.w(),
-              layout:{
-                top:29, left:153, width:300, height:31
-              },
-              contentBinding:'Nvzn.selectedRecord',
-              render:function (context) {
-                return context.text(this.getPath('content.displayName'));
-              }
-            }),
-            dateLabel:SC.View.extend({
-              classNames:'label date'.w(),
-              layout:{
-                top:35, left:394, width:90, height:18},
-              render:function (context, firstTime) {
-                return context.text('Week Ending:');
-              }
-            }),
-            dateView:SC.View.extend({
-              classNames:'date-value'.w(),
-              displayProperties:'content'.w(),
-              layout:{
-                top:29, left:496, width:300, height:31
-              },
-              contentBinding:'Nvzn.weekEnding',
-              render:function (context) {
-                var content = this.getPath('content');
-                var display = content ? content.toFormattedString('%A, %d %B %Y') : "";
-                return context.text(display);
-              }
-            }),
-            contactLabel:SC.View.extend({
-              classNames:'label contact'.w(),
-              isVisibleBinding:SC.Binding.from('Nvzn.isSite'),
-              layout:{
-                top:70, left:10, width:110, height:18},
-              render:function (context, firstTime) {
-                return context.text('Contact Details:');
-              }
-            }),
-            contactView: SC.View.extend({
-              classNames: 'header-contact'.w(),
-              layout: { top: 66, left: 130, right: 20, height: 24 },
-              displayProperties: 'content'.w(),
-              contentDisplayProperties: 'contactNumbers'.w(),
-              contentBinding: 'Nvzn.selectedRecord',
-              render: function(context) {
-                var numbers = this.getPath('content.contactNumbers');
-                return context.text(numbers ? numbers.without(" ").join(", ") : "");
-              }
-            }),
-            greetingLabel: SC.LabelView.extend({
-              layout: {right: 100, width: 300, height:24, top:23},
-              textAlign: SC.ALIGN_RIGHT,
-              valueBinding: SC.Binding.from('Nvzn.loginController.fullName')
-                .transform(function(value) {
-                  return "You are logged in as: "+value;
-                })
-            }),
-            logoutButton: SC.ButtonView.extend({
-              classNames: 'header-logout'.w(),
-              layout: {
-                right: 10,
-                top: 20,
-                width: 80,
-                height: 24
-              },
-              title: 'Logout',
-              target: Nvzn.statechart,
-              action: 'logout'
-            })
+        //// HEADER
+        headerView:SC.View.extend({
+
+          classNames:'page-header'.w(),
+          layout:{height:100},
+          childViews:'nameLabel nameView dateLabel dateView contactLabel contactView greetingLabel logoutButton'.w(),
+          nameLabel:SC.View.extend({
+            classNames:'label project'.w(),
+            isVisibleBinding:SC.Binding.from('Nvzn.isSite'),
+            layout:{top:35, left:40, width:110, height:18},
+            render:function (context, firstTime) {
+              return context.text('Name:');
+            }
           }),
-
-          // END HEADER
-
-          // TAB VIEW
-
-          contentView:SC.View.extend({
-            childViews:'tabView'.w(),
+          nameView:SC.View.extend(SC.ContentDisplay, {
+            classNames:'header-name'.w(),
+            displayProperties:'content'.w(),
+            contentDisplayProperties:'displayName'.w(),
+            layout:{top:29, left:153, width:300, height:31},
+            contentBinding:'Nvzn.selectedRecord',
+            render:function (context) {
+              return context.text(this.getPath('content.displayName'));
+            }
+          }),
+          dateLabel:SC.View.extend({
+            classNames:'label date'.w(),
             layout:{
-              top:105
+              top:35, left:394, width:90, height:18},
+            render:function (context, firstTime) {
+              return context.text('Week Ending:');
+            }
+          }),
+          dateView:SC.View.extend({
+            classNames:'date-value'.w(),
+            displayProperties:'content'.w(),
+            layout:{
+              top:29, left:496, width:300, height:31
+            },
+            contentBinding:'Nvzn.weekEnding',
+            render:function (context) {
+              var content = this.getPath('content');
+              var display = content ? content.toFormattedString('%A, %d %B %Y') : "";
+              return context.text(display);
+            }
+          }),
+          contactLabel:SC.View.extend({
+            classNames:'label contact'.w(),
+            isVisibleBinding:SC.Binding.from('Nvzn.isSite'),
+            layout:{top:70, left:10, width:110, height:18},
+            render:function (context, firstTime) {
+              return context.text('Contact Details:');
+            }
+          }),
+          contactView: SC.View.extend({
+            classNames: 'header-contact'.w(),
+            layout: { top: 66, left: 130, right: 20, height: 24 },
+            displayProperties: 'content'.w(),
+            contentDisplayProperties: 'contactNumbers'.w(),
+            contentBinding: 'Nvzn.selectedRecord',
+            render: function(context) {
+              var numbers = this.getPath('content.contactNumbers');
+              return context.text(numbers ? numbers.without(" ").join(", ") : "");
+            }
+          }),
+          greetingLabel: SC.LabelView.extend({
+            layout: {right: 100, width: 300, height:24, top:23},
+            textAlign: SC.ALIGN_RIGHT,
+            valueBinding: SC.Binding.from('Nvzn.loginController.fullName')
+              .transform(function(value) {
+                return "You are logged in as: "+value;
+              })
+          }),
+          logoutButton: SC.ButtonView.extend({
+            classNames: 'header-logout'.w(),
+            layout: {
+              right: 10,
+              top: 20,
+              width: 80,
+              height: 24
+            },
+            title: 'Logout',
+            target: Nvzn.statechart,
+            action: 'logout'
+          })
+        }),
+
+        // END HEADER
+
+        // TAB VIEW
+
+        contentView:SC.View.extend({
+          childViews:'tabView'.w(),
+          layout:{
+            top:105
+          },
+
+          tabView:SC.TabView.extend({
+            items:[],
+            nowShowing:'all',
+            itemTitleKey:'title',
+            itemValueKey:'value',
+            tabLocation:SC.BOTTOM_LOCATION,
+
+            nowShowingDidChange:function () {
+              Nvzn.statechart.sendEvent('tabDidChange');
+            }.observes('nowShowing'),
+
+            createChildViews:function () {
+              sc_super();
+
+              // fix the hardcoded layout
+
+              this.containerView.adjust({border:0, bottom:44});
+              this.segmentedView.adjust({height:44});
+
+              var indicator = this.createChildView(SC.View.extend({
+                classNames:['indicator'],
+                layout:{
+                  left:0,
+                  width:20,
+                  height:12,
+                  bottom:32
+                }
+              }));
+
+              this.set('indicatorView', indicator);
+              this.appendChild(indicator);
             },
 
-            tabView:SC.TabView.extend({
-              items:[],
-              nowShowing:'all',
-              itemTitleKey:'title',
-              itemValueKey:'value',
-              tabLocation:SC.BOTTOM_LOCATION,
+            didAppendToDocument:function () {
+              // fix the hard coded text-align
+              this.segmentedView.$().css('textAlign', 'left');
+              this._animateIndicator();
+            },
 
-              nowShowingDidChange:function () {
-                Nvzn.statechart.sendEvent('tabDidChange');
-              }.observes('nowShowing'),
-
-              createChildViews:function () {
-                sc_super();
-
-                // fix the hardcoded layout
-
-                this.containerView.adjust({border:0, bottom:44});
-                this.segmentedView.adjust({height:44});
-
-                var indicator = this.createChildView(SC.View.extend({
-                  classNames:['indicator'],
-                  layout:{
-                    left:0,
-                    width:20,
-                    height:12,
-                    bottom:32
-                  }
-                }));
-
-                this.set('indicatorView', indicator);
-                this.appendChild(indicator);
-              },
-
-              didAppendToDocument:function () {
-                // fix the hard coded text-align
-                this.segmentedView.$().css('textAlign', 'left');
-                this._animateIndicator();
-              },
-
-              _animateIndicator:function () {
-                // get the current tab button and use its frame to animate the arrow
-                var nowShowing = this.get('nowShowing'),
-                  tab = this.segmentedView.get('childViews').toArray().find(function (v) {
-                    return v.get('value') == nowShowing;
-                  });
-                if (tab) {
+            _animateIndicator:function () {
+              // get the current tab button and use its frame to animate the arrow
+              var nowShowing = this.get('nowShowing'),
+              tab = this.segmentedView.get('childViews').toArray().find(function (v) {
+                return v.get('value') == nowShowing;
+              });
+              if (tab) {
                 var frame = tab.get('frame'),
-                  newLeft = frame ? frame.x + (frame.width / 2) - 10 : 50;
+                newLeft = frame ? frame.x + (frame.width / 2) - 10 : 50;
 
                 this.indicatorView.animate('left', newLeft, {duration:0.3, timing:'ease'});
-                }
-              }.observes('nowShowing')
-            })
-          }),
+              }
+            }.observes('nowShowing')
+          })
+        }),
 
-          footerView:SC.View.extend({
+        footerView:SC.View.extend({
           childViews:'approveButton'.w(),
 
           layout:{
@@ -373,6 +368,8 @@ Nvzn.mainPage = SC.Page.create({
 
     })
   }),
+
+
 
   all_employees: EO.TableView.design({
     columns:[
