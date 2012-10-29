3f13cecf6f58b23f1095e31f712a7f63ebe4d4e2
geoffreyd <geoffreyd@gmail.com>
2012-10-29 12:25:49 +1100

    update versioner to use app name variable.

--- update-version.sh
+++ update-version.sh
@@ -2,8 +2,9 @@
 revisioncount=`git log --oneline | wc -l | tr -d ' '`
 projectversion=`git describe --tags --long`
 cleanversion=${projectversion%%-*}
+appname = 'nvzn'
 
 #echo "$projectversion-$revisioncount"
 #echo "$cleanversion.$revisioncount"
 
-sed -e "s/VERSION: '[0-9a-zA-Z\.-]*'/VERSION: '`echo "$projectversion-$revisioncount"`'/" -i .bak apps/nvzn/core.js
+sed -e "s/VERSION: '[0-9a-zA-Z\.-]*'/VERSION: '`echo "$projectversion-$revisioncount"`'/" -i .bak apps/$appname/core.js
